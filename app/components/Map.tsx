620cd893d5a0bdd016f62a75052cc776cc93ce26
noah7000 <noah.jackson@betheluniversity.edu>
2024-09-19 12:19:26 -0400

    Added plane landing zone markers, populated from a server-side json file.

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import landingZones from '../dataFiles/landingZones.json';
 
 interface MapProps {
   center: google.maps.LatLngLiteral;
@@ -9,10 +10,21 @@ interface MapProps {
 let map: google.maps.Map;
 
 export function addMarker(lat: number, lon: number) {
+
+  const pin = new google.maps.marker.PinElement({
+    scale: 1,
+    background: '#000FFF',
+    borderColor: '#000FFE',
+    glyphColor: 'white',
+  });
+
   if (map) {
-    new google.maps.marker.AdvancedMarkerElement({
+    let marker = new google.maps.marker.AdvancedMarkerElement({
       position: { lat, lng: lon }, 
       map: map,
+      content: pin.element,
+      title: "Title text for the test marker",
+      gmpClickable: true,
     });
   } else {
     console.error('Map instance not available');
@@ -23,19 +35,73 @@ const Map: React.FC<MapProps> = ({ center, zoom, mapId }) => {
 
   useEffect(() => {
     async function initMap(): Promise<void> {
+      //Api library imports
+      const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+      const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+      //Instantiates the map
       if (!map) {
         const { Map } = await google.maps.importLibrary('maps') as google.maps.MapsLibrary;
         map = new Map(document.getElementById('map') as HTMLElement, {
           center: center,
           zoom: zoom,
-          mapId: mapId
+          mapId: mapId,
+          mapTypeId: google.maps.MapTypeId.TERRAIN,
         });
       }
+
+      // Create an info window to share between markers.
+    const infoWindow = new InfoWindow();
+
+    // Create the markers.
+      landingZones.forEach(({position, title}, i) => {
+
+        // A switch case to determine the background color based on surface type
+        let backgroundColor;
+        switch (true) {
+          case title.includes('Grass'):
+            backgroundColor = '#27d836';
+            break;
+          case title.includes('Concrete'):
+            backgroundColor = '#a9a8ac';
+            break;
+          case title.includes('Gravel'):
+            backgroundColor = '#5d5c61';
+            break;
+          default:
+            backgroundColor = '#1979e6'; // Default color if no specific surface is found
+        }
+
+        // Custom styling for the Pins
+        const pin = new PinElement({
+            glyph: `${i + 1}`,
+            glyphColor: '#FFFFFF',
+            background: backgroundColor,
+            borderColor: 'black',
+            scale: 1.5,
+        });
+
+        // Marker creation
+        const marker = new AdvancedMarkerElement({
+            position,
+            map,
+            title: `${i + 1}. ${title}`,
+            content: pin.element,
+            gmpClickable: true,
+        });
+
+        // Click listener for each marker, and set up the info window.
+        marker.addListener('gmp-click', (event: google.maps.MapMouseEvent) => {
+          const latLng = event.latLng;
+          infoWindow.close();
+          infoWindow.setContent(marker.title);
+          infoWindow.open(marker.map, marker);
+        });
+      });
     }
 
     initMap();
   }, [center, zoom, mapId]);
-
 
   return (
       <>
