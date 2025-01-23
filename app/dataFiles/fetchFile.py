7346a6587ac042cf9e52ae5bc0846f3beaeff662
MattLloyd2027 <161510791+MattLloyd2027@users.noreply.github.com>
2025-01-23 18:15:58 -0500

    Added a list_files function

--- app/dataFiles/fetchFile.py
+++ app/dataFiles/fetchFile.py
@@ -7,9 +7,14 @@ client = storage.Client()
 
 
 
-def dowload_file(filename, bucketname, pathToSave):
+def dowload_file(filename, bucketname, pathToSave, client):
     bucket = client.get_bucket(bucketname)
 
     blob = bucket.blob(filename)
 
     blob.download_to_filename(pathToSave)
+
+def list_files(bucketName, client):
+    bucket = client.bucket(bucketName)
+    blobs = list(bucket.list_blobs())
+    print(blobs)
