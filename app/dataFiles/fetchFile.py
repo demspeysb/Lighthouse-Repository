f44d5e5de1c150f1db2ac59402edb803d4101809
MattLloyd2027 <matthew.lloyd@betheluniversity.edu>
2025-01-28 19:01:51 -0500

    fixed document lister

--- app/dataFiles/fetchFile.py
+++ app/dataFiles/fetchFile.py
@@ -7,7 +7,7 @@ client = storage.Client()
 
 
 
-def dowload_file(filename, bucketname, pathToSave, client):
+def download_file(filename, bucketname, pathToSave, client):
     bucket = client.get_bucket(bucketname)
 
     blob = bucket.blob(filename)
@@ -17,4 +17,9 @@ def dowload_file(filename, bucketname, pathToSave, client):
 def list_files(bucketName, client):
     bucket = client.bucket(bucketName)
     blobs = list(bucket.list_blobs())
+    i = 0
+    for blob in blobs:
+        blobs[i]=blobs[i].name
+        i = i+1
     print(blobs)
+list_files("docviewertestbucket", client)
