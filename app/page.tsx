44e67b7d6fa66c4061d0710cb5edc092464a6a73
noah7000 <noah.jackson@betheluniversity.edu>
2025-03-20 18:05:55 -0400

    cleaned testing code

--- app/page.tsx
+++ app/page.tsx
@@ -16,72 +16,13 @@ export default function PrivatePage() {
             <div id="checkbox-container" className="checkbox-container">
               <input type="checkbox" id="myCheckbox" name="myCheckbox" onChange={() => toggleMarkerGroup()}></input>
               <label htmlFor="myCheckbox">Plane Landing Zones</label>
-              <button onClick={async () => {
-                try {
-                  const response = await fetch('/api/fileViewer'); // Adjust the path as needed
-                  const data = await response.json();
-                  console.log(data.output); // You can do something with the data here
-                } catch (error) {
-                  await console.error('Error fetching data:', error);
-                }
-              }}>
-              ConnectionTest
-              </button>
-              {
-              <button onClick={async () => {
-                try {
-                  sendDataToBackend("Original_documents/ICS 202 Incident Objectives FILLABLE.pdf");
-                } catch (error) {
-                  await console.error('Error fetching data:', error);
-                }
-              }}>
-              ConnectionTestPOST
-              </button>
-              }
+              
             </div>
         </div>
-
         <div className="map-div">
           {<Map/>}
         </div>
       </div>
     </main>
   );
-};
-/*
-const sendStringToBackend = async () => {
-  const myString = "Hello from frontend!";
-
-  try {
-      const response = await fetch("http://localhost:3000/api/send-string", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ data: myString }),
-      });
-
-      const result = await response.json();
-      console.log("Response from backend:", result);
-  } catch (error) {
-      console.error("Error sending string:", error);
-  }
-};*/
-const sendDataToBackend = async (filename: string) => {
-  try {
-    const response = await fetch('../api/fileViewer', {
-      method: 'POST',
-      headers: {
-        'Content-Type': "text/plain", // specify JSON format
-      },
-      //body: JSON.stringify({ data: 'Hello from Front end' }), // pass the input data as JSON
-      body: filename, // pass the input data as JSON
-    });
-    console.log('We attempt the frontend post')
-    const result = await response.json(); // handle backend response if needed
-    console.log('Backend response:', result);
-    
-  } catch (error) {
-    console.error('Error sending data to backend:', error);
-  }
 };
