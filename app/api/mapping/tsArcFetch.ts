6973bf446d2786ef7a42c99847ab25191699f3a7
noah7000 <noah.jackson@betheluniversity.edu>
2025-03-27 23:08:21 -0400

    Push to solve a build error. Mostly just commented out WIP code related to fetching arcgis layers dynamically

--- app/api/mapping/tsArcFetch.ts
+++ app/api/mapping/tsArcFetch.ts
@@ -18,23 +18,22 @@ fetchEsriData('https://services2.arcgis.com/bHCR7EW2rtRWmtnn/ArcGIS/rest/service
 
 
 // Code for writing the ARCGIS geojson to a file
-/*
-async function fetchEsriDataToOutputFile(url: string, outputFile: string) {
-     try {
-         const response = await fetch(`${url}/query?where=1=1&outFields=*&outSR=4326&f=geojson`);
-         const data = await response.json();
 
-         // // Write data to a JSON file
-         await fs.writeFile(outputFile, JSON.stringify(data, null, 2));
-         console.log(`Data successfully written to ${outputFile}`);
-     } catch (error) {
-        console.error("Error fetching ArcGIS data:", error);
-     }
-}
+// async function fetchEsriDataToOutputFile(url: string, outputFile: string) {
+//      try {
+//          const response = await fetch(`${url}/query?where=1=1&outFields=*&outSR=4326&f=geojson`);
+//          const data = await response.json();
+
+//          // // Write data to a JSON file
+//          await fs.writeFile(outputFile, JSON.stringify(data, null, 2));
+//          console.log(`Data successfully written to ${outputFile}`);
+//      } catch (error) {
+//         console.error("Error fetching ArcGIS data:", error);
+//      }
+// }
 
 
-// // Example usage:
- const esriLayerUrl = "https://services2.arcgis.com/bHCR7EW2rtRWmtnn/ArcGIS/rest/services/EMS_BOUNDARIES/FeatureServer/21";
-fetchEsriDataToOutputFile(esriLayerUrl, "./app/api/mapping/GISDataLayer.json");
+// // // Example usage:
+//  const esriLayerUrl = "https://services2.arcgis.com/bHCR7EW2rtRWmtnn/ArcGIS/rest/services/EMS_BOUNDARIES/FeatureServer/21";
+// fetchEsriDataToOutputFile(esriLayerUrl, "GISDataLayer.json");
 
-*/
